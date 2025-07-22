e04052625632b54297b77251dea7eebdc2284a00
ardalan ebrahimi <ardalan.ebrahimi@gmail.com>
2025-07-23 01:25:25 +0200

    Escape JavaScript strings in environment variable replacements for safety

--- src/environments/environment.template.ts
+++ src/environments/environment.template.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  openaiApiKey: "YOUR_OPENAI_API_KEY_HERE",
-  mainPrompt: "YOUR_MAIN_PROMPT_HERE",
-  openAIFixInstructions: "YOUR_OPENAI_FIX_INSTRUCTIONS_HERE",
+  openaiApiKey: `YOUR_OPENAI_API_KEY_HERE`,
+  mainPrompt: `YOUR_MAIN_PROMPT_HERE`,
+  openAIFixInstructions: `YOUR_OPENAI_FIX_INSTRUCTIONS_HERE`,
 };

0cf09737a1192d2674ac497d962e2d63da5236c2
ardalan ebrahimi <ardalan.ebrahimi@gmail.com>
2025-07-27 00:24:42 +0200

    fix: add apiUrl to environment template

--- src/environments/environment.template.ts
+++ src/environments/environment.template.ts
@@ -1,5 +1,6 @@
 export const environment = {
   production: false,
+  apiUrl: `YOUR_API_URL_HERE`,
   openaiApiKey: `YOUR_OPENAI_API_KEY_HERE`,
   mainPrompt: `YOUR_MAIN_PROMPT_HERE`,
   openAIFixInstructions: `YOUR_OPENAI_FIX_INSTRUCTIONS_HERE`,
